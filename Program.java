1b994b7b03fa40fc55f490bf3ae6eb70de8da9ab
Akash Chauhan <49335824+Akash-Ch2103@users.noreply.github.com>
2019-04-12 11:23:47 +0530

    Project in java

--- Program.java
+++ Program.java
@@ -1,72 +1,32 @@
-#include<stdio.h>
- 
-int main()
+import java.util.*;
+class os
 {
-    int bt[20],p[20],wt[20],tat[20],pr[20],i,j,n,total=0,pos,temp,avg_wt,avg_tat;
-    printf("Enter Total Number of Process:");
-    scanf("%d",&n);
- 
-    printf("\nEnter Burst Time and Priority\n");
-    for(i=0;i<n;i++)
-    {
-        printf("\nP[%d]\n",i+1);
-        printf("Burst Time:");
-        scanf("%d",&bt[i]);
-        printf("Priority:");
-        scanf("%d",&pr[i]);
-        p[i]=i+1;           //contains process number
-    }
- 
-    //sorting burst time, priority and process number in ascending order using selection sort
-    for(i=0;i<n;i++)
-    {
-        pos=i;
-        for(j=i+1;j<n;j++)
-        {
-            if(pr[j]<pr[pos])
-                pos=j;
-        }
- 
-        temp=pr[i];
-        pr[i]=pr[pos];
-        pr[pos]=temp;
- 
-        temp=bt[i];
-        bt[i]=bt[pos];
-        bt[pos]=temp;
- 
-        temp=p[i];
-        p[i]=p[pos];
-        p[pos]=temp;
-    }
- 
-    wt[0]=0;	//waiting time for first process is zero
- 
-    //calculate waiting time
-    for(i=1;i<n;i++)
-    {
-        wt[i]=0;
-        for(j=0;j<i;j++)
-            wt[i]+=bt[j];
- 
-        total+=wt[i];
-    }
- 
-    avg_wt=total/n;      //average waiting time
-    total=0;
- 
-    printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
-    for(i=0;i<n;i++)
-    {
-        tat[i]=bt[i]+wt[i];     //calculate turnaround time
-        total+=tat[i];
-        printf("\nP[%d]\t\t  %d\t\t    %d\t\t\t%d",p[i],bt[i],wt[i],tat[i]);
-    }
- 
-    avg_tat=total/n;     //average turnaround time
-    printf("\n\nAverage Waiting Time=%d",avg_wt);
-    printf("\nAverage Turnaround Time=%d\n",avg_tat);
- 
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+//int bt[10],p[10],wt[10],tat[10],pr[10],at[10];
+int i,j,n,total=0,pos,temp,avg_wt,avg_tat;
+int[] bt=new int[10];
+int[] at=new int[10];
+int[] p=new int[10];
+int[] wt=new int[10];
+int[] tat=new int[10];
 
-	return 0;
+//int[] intArray = new int[20];
+
+System.out.println("Enter total number of process");
+n=sc.nextInt();
+System.out.println();
+System.out.println("Enter arrival time and burst time");
+System.out.println();
+for(i=0;i<n;i++)
+{
+System.out.println("P[" + i + "]");
+System.out.println("Enter arrival time:");
+at[i]=sc.nextInt();
+System.out.println("Enter burst time:");
+bt[i]=sc.nextInt();
+p[i]=i+1;
 }
+
+}}
